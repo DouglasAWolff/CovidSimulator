e7435bab7430e8bf1842a71297e5eae2e289b418
makobako <75222401+makobako@users.noreply.github.com>
2022-04-18 12:02:01 +0100

    nodes now infect other nodes, dosen't work properly

--- classes.py
+++ classes.py
@@ -36,11 +36,13 @@ class Person:
         self.forces = []
         self.mass = 10
         self.resistance = 1.08
-        self.velocity = [0,0]
+        self.velocity = [0, 0]
         self.selected = False
         self.size = 30 + (2.5 * len(self.connected_to))
         self.color = (150, 150 + (5 * len(self.connected_to)), 150 + (5 * len(self.connected_to)))
         self.screensize = screensize
+        self.infected = False
+        self.been_infected_for = 0
 
     def update(self, check_mouse=False):
         if check_mouse:
@@ -64,24 +66,33 @@ class Person:
             self.move_away_if_touching_walls()
             self.apply_velocity()
 
+        if self.infected:
+            self.color = (255,0,0)
+
+        else:
+            self.color = (150, 150 + (5 * len(self.connected_to)), 150 + (5 * len(self.connected_to)))
+
 
         self.size = 30 + (2.5 * len(self.connected_to))
 
-
-
     def draw(self):
-        self.app.fill(150, 150 + (5 * len(self.connected_to)), 150 + (5 * len(self.connected_to)))  # fills in the circle
-        self.app.ellipse(self.position[0], self.position[1], self.size, self.size)  # draws a circle at the position of the person
+        self.app.fill(self.color[0], self.color[1], self.color[2])  # fills in the circle
+        self.app.ellipse(self.position[0], self.position[1], self.size,
+                         self.size)  # draws a circle at the position of the person
 
-    def find_neighbours_in_approximate_distance(self, distance,
-                                                plusminus):  # takes in a number and then spits out that number of closest neighbours
+    def take_turn(self):
+        if self.been_infected_for > 1:
+            self.infect_others()
+
+        if self.infected:
+            self.been_infected_for += 1
+
+    def find_neighbours_in_approximate_distance(self, distance, plusminus):  # takes in a number and then spits out that number of closest neighbours
         people_distances = []
         for _person in self.people.people_array:
             people_distances.append([find_distance(self.position, _person.position), _person.index_in_people_array])
-        people_distances.sort()
 
         people_in_distance = [distance_person[1] for distance_person in people_distances if distance_person[0] < random.randint(distance - plusminus, distance + plusminus) and distance_person[1] != self.index_in_people_array]
-
 
         return people_in_distance
 
@@ -109,7 +120,6 @@ class Person:
         self.velocity[0] += total_force[0] / self.mass
         self.velocity[1] += total_force[1] / self.mass
 
-
     def apply_velocity(self):
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
@@ -117,14 +127,22 @@ class Person:
     def move_away_if_touching_walls(self):
         if self.position[0] - self.size < 0:
             self.velocity[0] += 50 / self.mass
-        if self.position[1]  - self.size < 0:
+        if self.position[1] - self.size < 0:
             self.velocity[1] += 50 / self.mass
         if self.position[0] + self.size > self.screensize[0]:
             self.velocity[0] -= 50 / self.mass
         if self.position[1] + self.size > self.screensize[1]:
             self.velocity[1] -= 50 / self.mass
 
+    def get_infected(self):
+        self.infected = True
+        print("infected")
 
+    def infect_others(self):
+        if self.infected:
+            for person in self.connected_to:
+                if random.randint(0,1):
+                    self.people.people_array[person].get_infected()
 
 
 class Connection:
@@ -135,7 +153,7 @@ class Connection:
         self.person2_index = person2
         self.coords1 = self.people.get_coords_for_person(person1)
         self.coords2 = self.people.get_coords_for_person(person2)
-        self.length_target = random.randint(200,250)
+        self.length_target = random.randint(200, 250)
         self.length = self.calculate_length()
         self.spring_constant = 0.08
 
@@ -175,7 +193,8 @@ class Connections:
         self.connections = []
         for first_person in range(len(self.people.people_array)):  # iterates through the people array, setting first person to the index
             for second_person in self.people.people_array[first_person].connected_to:  # iterates through the connections array of the first person
-                self.connections.append(Connection(first_person, second_person, self.people, self.app))  # appends a connection to the connections list
+                self.connections.append(Connection(first_person, second_person, self.people,
+                                                   self.app))  # appends a connection to the connections list
                 self.people.people_array[second_person].connected_to.append(first_person)
 
     def update(self):
@@ -196,16 +215,15 @@ class People:
         self.app = _app
         self.clicked = False
         self.person_selected = False
-        self.people_array = [
-            Person(screensize, [random.randint(0, screensize[0]), random.randint(0, screensize[1])], _app, self, i) for i in range(number_of_people)]  # fills the people array with people
+        self.people_array = [Person(screensize, [random.randint(0, screensize[0]), random.randint(0, screensize[1])], _app, self, i) for i in range(number_of_people)]  # fills the people array with people
 
         for person in self.people_array:
-            person.connected_to = person.find_neighbours_in_approximate_distance(300,0)    #[i for i in [random.choice(self.people_array).index_in_people_array for i in range(random.choice([1,1,1,1,1,1,2,3,4]))] if not i == person.index_in_people_array]
+            person.connected_to = person.find_neighbours_in_approximate_distance(300,0)  # [i for i in [random.choice(self.people_array).index_in_people_array for i in range(random.choice([1,1,1,1,1,1,2,3,4]))] if not i == person.index_in_people_array]
 
     def update(self):  # calls the update function on all the people
         if mouse.is_pressed(button='left'):
             if not self.person_selected:
-                self.clicked = True # could try putting a K-D tree here soon
+                self.clicked = True  # could try putting a K-D tree here soon
 
         for i in range(len(self.people_array)):
             self.people_array[i].update(self.clicked)
@@ -216,6 +234,16 @@ class People:
         for person in self.people_array:
             person.draw()
 
+    def take_turn(self):
+        for person in self.people_array:
+            person.take_turn()
 
     def get_coords_for_person(self, index):  # returns the coordinates for a person by index
         return self.people_array[index].position
+
+    def infect(self):
+        for person in self.people_array:
+            person.infect_others()
+
+    def infect_random_person(self):
+        random.choice(self.people_array).get_infected()
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import processing_py
 import classes
-import mouse
+import time
 
 screensize = [1800,1000]
 app = processing_py.App(screensize[0],screensize[1])  # create window: width, height
 people = classes.People(screensize, 40, app)
 connections = classes.Connections(people, app)
 
+people.infect_random_person()
+
+turn_timer = time.time()
 
 while True:
+    if time.time() - turn_timer > 1:
+        people.take_turn()
+        turn_timer = time.time()
+        print(f"turn,   {time.time()}")
+
+
     app.background(255, 255, 255)  # set background:  red, green, blue
 
 
